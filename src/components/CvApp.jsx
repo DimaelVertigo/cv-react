981805a3bf8c1cd9aa82a45f4d94635c21c64d17
Dmitry Klimenko <vertigo@yandex-team.ru>
2017-04-09 20:40:13 +0300

    Starter

---
+++ src/components/CvApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './CvApp.less';
+
+var CvApp = React.createClass({
+   
+    render: function() {
+        return (
+            <div className="cv-app">
+                cv-app
+            </div>
+        );
+    }
+});
+
+export default CvApp;
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './components/CvApp.jsx';
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('cv')
+);
